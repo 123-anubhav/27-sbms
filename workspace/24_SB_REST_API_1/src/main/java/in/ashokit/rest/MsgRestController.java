874a454e0d6218569fb17b9abfb18eba6056f9ac
123-anubhav <anubhav.aa2@gmail.com>
2024-12-18 21:59:10 +0530

    boot and microservices notes and practice projects

---
+++ workspace/24_SB_REST_API_1/src/main/java/in/ashokit/rest/MsgRestController.java
@@ -0,0 +1,29 @@
+package in.ashokit.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RestController
+//@CrossOrigin(origins = "*")
+public class MsgRestController {
+
+	public MsgRestController() {
+		System.out.println("MsgRestController::Constructor");
+	}
+
+	@GetMapping("/welcome")
+	public ResponseEntity<String> getWelcomeMsg() {
+		String msg = "Welcome To Ashok IT";
+		return new ResponseEntity<>(msg, HttpStatus.OK);
+	}
+
+	@GetMapping("/greet")
+	public String getGreetMsg() {
+		String msg = "Good Evening..!!";
+		return msg;
+	}
+
+}
